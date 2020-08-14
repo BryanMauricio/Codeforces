6f8acd204efd763e38237ab98d0a8dc4c9fd2378
Bryan Mauricio <bryabmauricio@gmail.com>
2020-08-13 13:31:00 -1100

    Add solution for problem `B - High School: Become Human` Link: https://codeforces.com/contest/987/problem/B Tags: math, *1100 Ref: https://codeforces.com/contest/987/submission/89832306

---
+++ codeforces/987/B.py
@@ -0,0 +1,40 @@
+import sys
+
+a, b = map(int,input().split())
+
+if a == b:
+    print("=")
+    sys.exit()
+
+if a < b and b > 1000:
+    print(">")
+    sys.exit()
+
+if a > b and b != 1 and a > 1000:
+    print("<")
+    sys.exit()
+
+if a > 100000 and b > 2:
+    print("<")
+    sys.exit()
+
+if a > 1000 and b > 1000:
+    if a < b:
+        print(">")
+        sys.exit()
+    elif a > b:
+        print("<")
+        sys.exit()
+    elif a == b:
+        print("=")
+        sys.exit()
+
+wynik_a = a ** b
+wynik_b = b ** a
+
+if wynik_a > wynik_b:
+  print(">")
+elif wynik_a < wynik_b:
+  print("<")
+elif wynik_a == wynik_b:
+  print("=")
