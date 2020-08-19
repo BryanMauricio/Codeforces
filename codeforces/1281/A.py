ea3170673a9b295c6e1a620f6b78949572472b83
Bryan Mauricio <bryabmauricio@gmail.com>
2020-08-19 02:11:00 -1100

    Add solution for problem `A - Suffix Three` Link: https://codeforces.com/contest/1281/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1281/submission/90390856

---
+++ codeforces/1281/A.py
@@ -0,0 +1,8 @@
+for _ in range(int(input())):
+    a = input()
+    if a[-1] == "o":
+        print("FILIPINO")
+    elif a[-1] == "u":
+        print("JAPANESE")
+    elif a[-1] == "a":
+        print("KOREAN")
