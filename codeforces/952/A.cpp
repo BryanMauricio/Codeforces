86ac0052af8d2262197896a6f58a422786c679de
Bryan Mauricio <bryabmauricio@gmail.com>
2020-08-22 05:00:00 -1100

    Add solution for problem `A - Quirky Quantifiers` Link: https://codeforces.com/contest/952/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/952/submission/90701734

--- codeforces/952/A.cpp
+++ codeforces/952/A.cpp
@@ -1,24 +1,17 @@
-#include<bits/stdc++.h>
-#include<iostream>
-#include<cstring>
-#include<iomanip>
-#include<fstream>
+#include <iostream>
 
 using namespace std;
 
 int main()
-
 {
-
-
     int x;
     cin >> x;
-    if(x % 2 == 0)
-        cout << 0;
-    else
-        cout << 1;
-
-
+    if(x % 2 == 0) {
+        cout << "0" << endl;
+    }
+    else {
+        cout << "1" << endl;
+    }
 
 
 
