60ab81d3a0c6fa961b4a133ea96c3f72429b5f37
Bryan Mauricio <bryabmauricio@gmail.com>
2020-08-23 08:40:00 -1100

    Add solution for problem `A - Expression` Link: https://codeforces.com/contest/479/problem/A Tags: brute force, math, *1000 Ref: https://codeforces.com/contest/479/submission/90781986

--- codeforces/479/A.cpp
+++ codeforces/479/A.cpp
@@ -13,18 +13,16 @@ int main()
     cin >> a;
     cin >> b;
     cin >> c;
-    int ua = 0;
-    int jk = 0;
     int x = a + (b * c);
     int y = a * (b + c);
     int u = a * b * c;
     int k = (a + b) * c;
     int h = a + b + c;
-    int lucky[] = {x, y, u, k, h};
-    int ha = max(jk, ua);
-    int *i1;
-    i1 = max_element(lucky, lucky + 5);
-    cout <<*i1;
+    int ok = max(x, y);
+    int oh = max(u, k);
+    int oj = max(ok , oh);
+
+    cout << max(oj,h);
 
     return 0;
 
