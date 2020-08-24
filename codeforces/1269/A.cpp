7de1f254b5d992841b4eab6563b9eedc997ab649
Bryan Mauricio <bryabmauricio@gmail.com>
2020-08-24 02:09:00 -1100

    Add solution for problem `A - Equation` Link: https://codeforces.com/contest/1269/problem/A Tags: brute force, math, *800 Ref: https://codeforces.com/contest/1269/submission/90828009

---
+++ codeforces/1269/A.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include<cstring>
+#include<iomanip>
+#include<fstream>
+
+using namespace std;
+
+int main()
+
+{
+
+
+    int x;
+    cin >> x;
+    int a = x * 9;
+    int b = a - x;
+    cout << a << " " << b;
+
+
+
+
+
+return 0;
+}
