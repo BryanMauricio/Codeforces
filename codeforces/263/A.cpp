dee7ba2b3b498d6405cffacd76b8f56a2932e2ad
Bryan Mauricio <bryabmauricio@gmail.com>
2020-08-24 02:36:00 -1100

    Add solution for problem `A - Beautiful Matrix` Link: https://codeforces.com/contest/263/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/263/submission/90830218

--- codeforces/263/A.cpp
+++ codeforces/263/A.cpp
@@ -8,11 +8,6 @@ using namespace std;
 
 int main()
 {
-    
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    
-    
     int a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y;
     cin >> a >> b >> c >> d >> e;
     cin >> f >> g >> h >> i >> j;
