d9066a8e7a1940932bd22e5bbbee1f61aca06a49
Bryan Mauricio <bryabmauricio@gmail.com>
2020-08-26 10:32:00 -1100

    Add solution for problem `A - Restoring Three Numbers` Link: https://codeforces.com/contest/1154/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1154/submission/91091931

---
+++ codeforces/1154/A.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    cout.setf(ios::fixed);
+
+    int a,b,c,d;
+    cin >> a >> b >> c >> d;
+    double values[4] = {a, b, c ,d};
+    double max = values[0];
+    for(int i = 0; i < 4; i++) {
+      if(values[i] > max) {
+        max = values[i];
+      }
+    }
+    if(max == d) {
+      cout << int(max  -a) << " " << int(max - b) << " " << int(max - c);
+    }
+    if(max == c) {
+      cout << int(max - a) << " " << int(max - b) << " " << int(max - d);
+    }
+    if(max == b) {
+      cout << int(max) - a << " " << int(max - c) << " " << int(max - d);
+    }
+    if(max == a) {
+      cout << int(max - b) << " " << int(max - c) << " " << int(max - d);
+    }
+
+}
