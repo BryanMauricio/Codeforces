9e4c0fb39ce07a65ff1bb3fd1d9701eea59f2f1c
Bryan Mauricio <bryabmauricio@gmail.com>
2020-08-26 02:34:00 -1100

    Add solution for problem `A - Elephant` Link: https://codeforces.com/contest/617/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/617/submission/91056896

--- codeforces/617/A.cpp
+++ codeforces/617/A.cpp
@@ -1,17 +1,22 @@
+#include <bits/stdc++.h>
 #include <iostream>
-#include <cmath>
-
 using namespace std;
-
 
 int main()
 {
-    int t;
-    cin >> t;
-    while(t % 5 != 0) {
-        t++;
-    }
-    cout << t/5;
+        int a;
+        cin >> a;
+        int b = 5;
+        while(b--) {
+            if(a % 5 == 0) {
+                cout << a / 5;
+                return 0;
+            }
+            else {
+                a++;
+            }
+
+        }
 
 
 }
