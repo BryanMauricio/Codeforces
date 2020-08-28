2e7fe06e1a8758bd8cd3cd935bea940ef12123b9
Bryan Mauricio <bryabmauricio@gmail.com>
2020-08-28 06:54:00 -1100

    Add solution for problem `A - Soft Drinking` Link: https://codeforces.com/contest/151/problem/A Tags: implementation, math, *800 Ref: https://codeforces.com/contest/151/submission/91234477

--- codeforces/151/A.cpp
+++ codeforces/151/A.cpp
@@ -6,10 +6,6 @@ using namespace std;
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(0);
-    
     int n, k, l, c, d, p, nl, np;
     cin >> n >> k >> l >> c >> d >> p >> nl >> np;
     int mil =  k * l / nl;
