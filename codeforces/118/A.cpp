a62450e377e36d59d21f96934b303ad0ee191834
Bryan Mauricio <bryabmauricio@gmail.com>
2020-08-30 05:16:00 -1100

    Add solution for problem `A - String Task` Link: https://codeforces.com/contest/118/problem/A Tags: implementation, strings, *1000 Ref: https://codeforces.com/contest/118/submission/91410185

--- codeforces/118/A.cpp
+++ codeforces/118/A.cpp
@@ -4,21 +4,29 @@
 #include <cctype>
 #include <string>
 
-
 using namespace std;
+
 
 
 
 int main()
 {
-
     string my_str;
     cin >> my_str;
     transform(my_str.begin(), my_str.end(), my_str.begin(), ::tolower);
-    for(int i = 0;i<my_str.size();i++){
-        if(my_str[i] != 'a' && my_str[i] != 'u' && my_str[i] != 'e' && my_str[i] != 'y' && my_str[i] != 'i' && my_str[i] != 'o') {
-            cout << "." << my_str[i];
-        }
+    my_str.erase(remove(my_str.begin(), my_str.end(), 'a'), my_str.end());
+    my_str.erase(remove(my_str.begin(), my_str.end(), 'u'), my_str.end());
+    my_str.erase(remove(my_str.begin(), my_str.end(), 'e'), my_str.end());
+    my_str.erase(remove(my_str.begin(), my_str.end(), 'i'), my_str.end());
+    my_str.erase(remove(my_str.begin(), my_str.end(), 'o'), my_str.end());
+    my_str.erase(remove(my_str.begin(), my_str.end(), 'y'), my_str.end());
+    
+    int n = my_str.size();
+    for(int i = 0;i<n;i++) {
+        cout << "." << my_str[i];
+
     }
+
+
 
 }
