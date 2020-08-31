089095447e33a98091f9bd54d23bab45fa69f138
Bryan Mauricio <bryabmauricio@gmail.com>
2020-08-31 07:52:00 -1100

    Add solution for problem `A - Modular Exponentiation` Link: https://codeforces.com/contest/913/problem/A Tags: implementation, math, *900 Ref: https://codeforces.com/contest/913/submission/91518556

---
+++ codeforces/913/A.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+#include <algorithm>
+#include<cmath>
+
+using namespace std;
+
+
+
+
+int main()
+{
+
+    int a,b;
+    cin >> a >> b;
+    int c = pow(2, a);
+    int wyn = b % c;
+    cout << wyn;
+
+
+}
