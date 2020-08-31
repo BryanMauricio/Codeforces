6e6c2929967fa1e3ed0be047d3283fbb7c3660b1
Bryan Mauricio <bryabmauricio@gmail.com>
2020-08-31 05:05:00 -1100

    Add solution for problem `A - Equalize Prices Again` Link: https://codeforces.com/contest/1234/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1234/submission/91507685

--- codeforces/1234/A.cpp
+++ codeforces/1234/A.cpp
@@ -1,31 +1,28 @@
 #include <iostream>
-#include <algorithm>
-#include <cmath>
-
 
 using namespace std;
 
-
 int main()
 {
-	int t,o[100000];
-	int sum = 0;
-	cin >> t;
-	while(t--) {
-		int a;
-		cin >> a;
-		for(int i = 0;i<a;i++) {
-			cin >> o[i];
-			sum += o[i];
-		}
-		if(sum % a == 0) {
-			cout << sum / a << endl;
-		}
-		else {
-			cout << sum / a + 1 << endl;
-		}
-		sum = 0;
-		
-	}	
-		
+
+    int t,sum=0,o[10000];
+    cin >> t;
+    while(t--) {
+        int x;
+        cin >> x;
+        for(int i =0;i<x;i++){
+            cin >> o[i];
+            sum += o[i];
+        }
+        if(sum % x == 0) {
+            cout << sum / x << endl;
+        }
+        else {
+            cout << sum / x + 1 << endl;
+        }
+        sum = 0;
+
+
+    }
+
 }
